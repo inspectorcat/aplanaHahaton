7c0dd68311892bed686d98b23d85ec3462cf1e0b
Tigran <tmantashyan@mail.ru>
2019-08-21 23:09:09 +0300

    Add redirect to REST-like page from GUI version page Refactor controller

--- src/main/java/com/aplana/vin/VinController.java
+++ src/main/java/com/aplana/vin/VinController.java
@@ -10,30 +10,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import redis.clients.jedis.JedisPoolConfig;
 
 import javax.annotation.PostConstruct;
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class VinController {
     private static JedisPoolConfig poolConfig = new JedisPoolConfig();
     private static GeneratorVin gv;
-    private static Map<String, String> map = new HashMap<>();
 
     @Autowired
     private RedisConfig data;
 
     // Get request
     @GetMapping("/generatevin")
-    public Map<String, String> vin() {
-        map.put("vin", gv.generateVIN());
-        return map;
+    public String vin(Model model) {
+        model.addAttribute("vin", gv.generateVIN());
+        return "generatevin";
     }
 
     // Get request
-    @GetMapping("/cat")
-    public String vinGui(Model model) {
-        model.addAttribute("vin", gv.generateVIN());
-        return "cat";
+    @GetMapping("/trytogenerate")
+    public String vinGui() {
+        return "trytogenerate";
     }
 
 
